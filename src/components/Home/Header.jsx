c9f6d7e7133c49362f109501e86796980f17bd4a
navjot kaur <navu77196@gmail.com>
2025-04-24 11:09:44 +0530

    logo/title/change

--- src/components/Home/Header.jsx
+++ src/components/Home/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import logo from '../../assets/logo.webp'
 
 function Header() {
   return (
-    <header className="bg-blue-900 py-8">
-      <div className="container mx-auto px-4">
-        <h1 className="text-center text-white text-3xl md:text-4xl font-bold tracking-widest">
+    <header className="bg-blue-900 py-3">
+      <div className="container flex justify-center gap-10 mx-auto px-4">
+        <img src={logo} className='w-20 h-20' alt="" />
+        <h1 className="text-center text-white text-3xl py-4 md:text-4xl font-bold tracking-widest">
           DORLABS TECH
         </h1>
       </div>
