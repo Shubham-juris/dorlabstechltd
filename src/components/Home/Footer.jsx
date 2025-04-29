8769bed8a9ff1f1601556f35c12f1962557665e2
Shubham Kumar <shubham.developerjuris@gmail.com>
2025-04-29 13:12:34 +0530

    Update Footer.jsx

--- src/components/Home/Footer.jsx
+++ src/components/Home/Footer.jsx
@@ -1,11 +1,11 @@
 export default function Footer() {
-    const currentYear = new Date().getFullYear();
+    
     
     return (
       <footer className="bg-blue-900 text-white py-10 w-full">
         <div className="container mx-auto text-center">
           <p className="text-sm uppercase tracking-wide">
-            COPYRIGHT © {currentYear} DORLABS TECH - ALL RIGHTS RESERVED.
+            COPYRIGHT © 2023 DORLABS TECH - ALL RIGHTS RESERVED.
           </p>
         </div>
       </footer>
